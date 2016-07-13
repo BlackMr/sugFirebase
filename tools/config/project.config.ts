d114c7f502aa8fbe30ce307a402c233059dde2dd
BlackMr <karabeykaan@gmail.com>
2016-07-13 23:18:05 +0300

    setup

---
+++ tools/config/project.config.ts
@@ -0,0 +1,26 @@
+import { join } from 'path';
+import { SeedAdvancedConfig } from './seed-advanced.config';
+
+/**
+ * This class extends the basic seed configuration, allowing for project specific overrides. A few examples can be found
+ * below.
+ */
+export class ProjectConfig extends SeedAdvancedConfig {
+
+  PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
+
+  constructor() {
+    super();
+    // this.APP_TITLE = 'Put name of your app here';
+
+    // Add third-party libraries to be injected/bundled.
+    this.NPM_DEPENDENCIES = [
+      ...this.NPM_DEPENDENCIES,
+      // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+    ];
+
+    /* Add to or override NPM module configurations: */
+    //this.mergeObject(this.PLUGIN_CONFIGS['browser-sync'], { ghostMode: false });
+  }
+
+}
